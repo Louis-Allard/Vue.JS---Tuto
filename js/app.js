84d870f23b470e63557c9ad59e29062fe7d99e8e
Louis Allard <la.devweb@yahoo.com>
2020-02-12 17:14:31 +0100

    Add button and timer.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-new Vue ({
+let vm = new Vue ({
     el: '#app',
     data: {
         message: 'Salut les gens',
         link: 'https://louis-allard.github.io/Portfolio/',
         success: true,
         personns : ['Louis','Aude','Maximilien','Aurélien'],
+        seconds: 0
+    },
+    mounted: function() {
+        setInterval(() => {
+            this.seconds++;
+        },1000)
     },
     methods: {
         close: function() {
             this.success = false,
-            this.message = 'Fermé'
+            this.message = 'Warning'
+        },
+        addPersonn: function() {
+            this.personns.push("Samuel");
         }
     }
 })
