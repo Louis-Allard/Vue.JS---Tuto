3507af550f2db39cc1a386cd7cd2784aa42ecfd8
Louis Allard <la.devweb@yahoo.com>
2020-02-12 10:15:45 +0100

    Add v-if and fix meta.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 new Vue ({
     el: '#app',
     data: {
-        message: 'Salut les gens'
+        message: 'Salut les gens',
+        link: 'https://louis-allard.github.io/Portfolio/',
+        success: true
     }
 })
